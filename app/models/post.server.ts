daa657da6c846901168270af3431cb4d8e2eb9ea
Nguyễn Ngọc Tuấn <tuannn3@onemount.com>
2022-05-29 20:13:50 +0700

    Initial

---
+++ app/models/post.server.ts
@@ -0,0 +1,7 @@
+export async function getPosts() {
+  const posts = [
+    { slug: "my-first-post", title: "My First Post!" },
+    { slug: "my-second-post", title: "My Second Post!" },
+  ];
+  return posts;
+}
