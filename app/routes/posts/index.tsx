cebd04df3a55ebeaaa4bacd27086726102e0d185
Nguyễn Ngọc Tuấn <tuannn3@onemount.com>
2022-06-12 15:58:43 +0700

    Create an Admin User Hook

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 import { json, LoaderFunction } from "@remix-run/node";
 
 import { getPostsListing } from "~/models/post.server";
+import { useOptionalAdminUser } from "~/utils";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPostsListing>>;
@@ -14,17 +15,26 @@ export const loader: LoaderFunction = async () => {
 
 export default function PostsRoute() {
   const { posts } = useLoaderData() as LoaderData;
-  console.log(posts);
+
+  const adminUser = useOptionalAdminUser();
+
+  console.log(adminUser);
   return (
     <main>
       <h1>Posts</h1>
-      <Link to="admin" className="text-red-600 underline">
-        Admin
-      </Link>
+      {adminUser ? (
+        <Link to="admin" className="text-red-600 underline">
+          Admin
+        </Link>
+      ) : null}
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link to={post.slug} className="text-blue-600 underline">
+            <Link
+              to={post.slug}
+              prefetch="intent"
+              className="text-blue-600 underline"
+            >
               {post.title}
             </Link>
           </li>
